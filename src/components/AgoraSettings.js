f6b19de2140e585c56328d18784805ad95697c2d
therealmcfly <eugene.fly.lee@gmail.com>
2022-12-01 16:16:42 +0900

    commit

--- src/components/AgoraSettings.js
+++ src/components/AgoraSettings.js
@@ -1,10 +1,10 @@
 import { createClient, createMicrophoneAudioTrack } from "agora-rtc-react";
 
-const appId = "aa82696e8d3345d1a0fae3906bd60c12";
+const appId = "b240b172a68a486496d8262bcc38d89c";
 const token =
-  "007eJxTYFgz++zuWQ9mVSyPVJu8SzBeIePFcWkVlZqQupwj4VwbZn5WYEhMtDAyszRLtUgxNjYxTTFMNEhLTDW2NDBLSjEzSDY02jGpNbkhkJFh6tfpLIwMEAjiCzKkZealZJboJufnpaUWpeYlpzIwAABifSW+";
+  "007eJxTYBDMLz725cfTZfNzlp6rCwzN2Va2ptZEIDPg/z6vqcE3+g8qMCQZmRgkGZobJZpZJJpYmJlYmqVYGJkZJSUnG1ukWFgmJwV3JDcEMjKseBHFxMgAgSA+G0NaZl5KZgkDAwAp2CIK";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAudioTrack = createMicrophoneAudioTrack();
-export const channelName = "findit-conference";
+export const channelName = "findit";
